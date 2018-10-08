897db7e2a3b5cf4b5e6115f2e9dbad8de8167d9c
Neel <send2neelpatel@gmail.com>
2018-10-08 01:30:42 -0400

    made 3 files

---
+++ TSserver.py
@@ -0,0 +1,27 @@
+import socket as mysoc
+
+try:
+	tssd = mysoc.socket(mysoc.AF_INET,mysoc.SOCK_STREAM)
+except mysoc.error as err:
+	print('[TS]: {}	\n '.format("socket open error ",err))
+
+
+## GENERAL SETUP
+# Select Port number | Host Name | LocalHost IP
+server_binding = ('', 50007)
+myHost = mysoc.gethostname()
+localhost_ip = (mysoc.gethostbyname(myHost))
+
+# bind to socket
+tssd.bind(server_binding)
+
+ctsd, addr = tssd.accept()
+
+# hnstring = hostname_str
+hostnameStr = ctsd.recv()
+if (hostnameStr in TS_table):
+	entry = TS_table(hostnameStr)
+else:
+	entry = "hname" + "Error: Host not found"
+
+ctsd.send(entry)
